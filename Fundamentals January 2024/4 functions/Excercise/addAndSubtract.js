7e810d1b8ae0f7d93ea8a4bbe8c23ed3f699e4c3
mostfresh <mostfresh@abv.bg>
2024-03-25 20:01:39 +0200

    some problems on functions

---
+++ Fundamentals January 2024/4 functions/Excercise/addAndSubtract.js
@@ -0,0 +1,12 @@
+function sum(a,b){
+    sumResult = a +b;
+    
+}
+ let sumResult = 0;
+
+function sub(c){
+    let subResult = sumResult - c
+    console.log(subResult);
+}
+sum(1,17)
+sub(30)
---
+++ Fundamentals January 2024/4 functions/Excercise/smallestOfThreeNumbers.js
@@ -0,0 +1,11 @@
+function smallest(a,b,c){
+
+    let smallestNumber = Math.min(a, b, c);
+    console.log(smallestNumber);
+}
+smallest(2,5,3)
+smallest(600,
+    342,
+    123)
+smallest(35,21,4)
+smallest(2,2,2)
